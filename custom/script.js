f779bea1a4603b204cfd41eda74676cb6583e81a
Henrique Santos <sant.gp.henrique@gmail.com>
2024-03-01 17:51:26 -0300

    VersaoWeb

--- custom/script.js
+++ custom/script.js
@@ -4,6 +4,24 @@ const separa = document.getElementById('separa');
 
 function handleClick(){
     buttonMenu.classList.toggle('active');
-    menu.classList.toggle('active');
+    menu.classList.toggle('active'); 
     separa.classList.toggle('active');
+}
+
+function sobreMim(){
+    document.getElementById('sobre_mim').style.display = "block";
+    document.getElementById('curriculo').style.display = "none";
+    document.getElementById('contato').style.display = "none";
+}
+
+function curriculo(){
+    document.getElementById('sobre_mim').style.display = "none";
+    document.getElementById('curriculo').style.display = "block";
+    document.getElementById('contato').style.display = "none";
+}
+
+function contato(){
+    document.getElementById('sobre_mim').style.display = "none";
+    document.getElementById('curriculo').style.display = "none";
+    document.getElementById('contato').style.display = "block";
 }
